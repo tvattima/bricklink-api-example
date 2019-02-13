037d47370b88737792a4dc71ee53136814e7bc32
tvattima <github@vattima.com>
2019-02-12 19:59:35 -0500

    - Refactor Lego Inventory Sheet Service and support classes

--- src/main/java/com/vattima/lego/sheet/meta/CellDescriptor.java
+++
@@ -1,6 +0,0 @@
-package com.vattima.lego.sheet.meta;
-
-public interface CellDescriptor<T> {
-    void setValue(Object v);
-    T getValue();
-}
--- src/main/java/com/vattima/lego/sheet/service/GoogleSheetsService.java
+++
@@ -1,7 +0,0 @@
-package com.vattima.lego.sheet.service;
-
-import com.google.api.services.sheets.v4.Sheets;
-
-public interface GoogleSheetsService {
-    Sheets getSheetsService();
-}
